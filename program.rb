a5ad33a7b97edd0e410b9e2de7024c9fa08c9122
learn <eric.desimone@cortland.edu>
2018-11-05 12:43:44 -0500

    done

--- program.rb
+++ program.rb
@@ -36,19 +36,11 @@ def get_unique_characters(paragraph)
 
   paragraph_hash = paragraph_hash.sort_by{|key,value| value}.to_h
 
-  stop_loop = false
-
-  while stop_loop == false
-    paragraph_hash.each do |key,value|
-      if total_count(paragraph_hash) - value > 50
-        paragraph_hash.delete(key) && unique_characters << key
-      else
-        stop_loop = true
-      end
-    end
+  paragraph_hash.each do |key,value|
+    paragraph_hash.delete(key) && unique_characters << key if total_count(paragraph_hash) - value > 50
   end
 
-puts unique_characters
+  puts unique_characters
 end
 
 get_unique_characters(paragraph)
